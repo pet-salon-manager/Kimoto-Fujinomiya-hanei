5b94d1e07a5c190b9932ed3b91ec7ad0cba7f2e6
pet-salon-manager <sinntyoukoukichild@yahoo.co.jp>
2026-09-18 07:42:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
-const CACHE='fujinomiya-kanko-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='fujinomiya-kanko-v2';
+const ASSETS=[
+  './','./index.html','./manifest.webmanifest','./apple-touch-icon.png',
+  './icon-192.png','./icon-512.png','./icon-1024.png','./fujinomiya-bg.jpg'
+];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
